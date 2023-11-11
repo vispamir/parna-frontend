0e258235c478a02b4ec329fca137bfa14d379fba
Amir Koulivand <vispa.amir@gmail.com>
2023-11-11 17:08:28 +0330

    Add parna logos.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/images/logo/parna-logo-b.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/parna-logo-w.svg"
                   alt="logo"
                   width={140}
                   height={30}
